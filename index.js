f56fad963872c7a1282c3530be4cdcfe0a695d47
Oyinda David <oyetunmibidavid9@gmail.com>
2019-06-24 14:55:57 +0100

    communicating with GitHub API

---
+++ index.js
@@ -0,0 +1,30 @@
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const files = require('./lib/files');
+const inquirer = require('./lib/inquirer');
+const github = require('./lib/github');
+
+clear();
+console.log(
+    chalk.yellow(
+        figlet.textSync('Git Flow', { horizontalLayout: 'full' })
+    )
+);
+
+// if (files.directoryExists('.git')) {
+//     console.log(chalk.red('Already a git repository!'));
+//     process.exit();
+// }
+
+// user interaction
+const run = async () => {
+    let token = github.getStoredGithubToken();
+    if(!token) {
+      await github.setGithubCredentials();
+      token = await github.registerNewToken();    
+    }
+    console.log(token);
+}
+
+run();
